2b6393202880a51564ec09ef48b1cea838a46c3f
Daniel Azevedo <49279228+dani-77@users.noreply.github.com>
2025-11-24 17:15:14 +0000

    Change key binding from 'd' to 'a' for rofi

--- chadwm/config.h
+++ chadwm/config.h
@@ -150,7 +150,7 @@ static const Key keys[] = {
     {MODKEY,                            XK_u,       spawn,
         SHCMD("maim --select | xclip -selection clipboard -t image/png")},
 
-    { MODKEY,                           XK_d,       spawn,          SHCMD("rofi -show drun") },
+    { MODKEY,                           XK_a,       spawn,          SHCMD("rofi -show drun") },
     { MODKEY,                           XK_x,       spawn,          SHCMD("rofi -show power-menu -modi power-menu:rofi-power-menu") },
     { MODKEY,                           XK_Return,  spawn,          SHCMD("st")},
 
